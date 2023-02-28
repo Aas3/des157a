94fd1a67750ae24add3d32f6543ca23355c2e7ee
Aas3 <aasaid@ucdavis.edu>
2023-02-28 00:13:56 -0800

    everypic

---
+++ studio2/script.js
@@ -0,0 +1,27 @@
+(function () {
+
+    'use strict';
+
+    console.log('reading js');
+
+
+	const container = document.querySelector('div');
+    const theImg = document.querySelector('div img');
+    const percent = container.offsetWidth / 100;
+    let prevLoc = 0;
+
+    container.addEventListener('mousemove', reportPos);
+
+    function reportPos(e){
+        const mousePosX = Math.ceil((e.clientX - (container.getBoundingClientRect().left)) / percent);
+        
+            if (prevLoc !== mousePosX){
+                const addedPx = mousePosX * 30;
+                theImg.style.width = (800 + addedPx) + 'px';
+                prevLoc = mousePosX;
+                console.log(prevLoc);
+            }
+    }
+
+
+})();
