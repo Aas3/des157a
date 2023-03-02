2e8ef4c21eb6d1314eec9ba530c7fe49fe883282
Aas3 <aasaid@ucdavis.edu>
2023-03-02 12:22:35 -0800

    new ep

--- studio2/script.js
+++ studio2/script.js
@@ -5,7 +5,31 @@
     console.log('reading js');
 
 
-	const container = document.querySelector('div');
+////////////// OVERLAYS ///////////////
+
+
+    // fade in as you scroll down
+    document.addEventListener('scroll', function(e){
+        e.preventDefault();
+        document.getElementById('sec1').className = 'showing';
+    });
+
+    document.addEventListener('scroll', function(e){
+        e.preventDefault();
+        document.getElementById('sec2').className = 'showing';
+    });
+
+
+
+
+
+ ////////////// SCROOL & MOUSE MOVEMENTS ///////////////
+
+
+    //zooms in when mouse comes in from left  
+    const container = document.getElementById('img1');
+    
+    // const container2 = document.getElementById('img2');
     const theImg = document.querySelector('div img');
     const percent = container.offsetWidth / 100;
     let prevLoc = 0;
@@ -22,6 +46,17 @@
                 console.log(prevLoc);
             }
     }
+
+    // function reportPos(e){
+    //     const mousePosX = Math.ceil((e.clientX - (container.getBoundingClientRect().right)) / percent);
+        
+    //         if (prevLoc !== mousePosX){
+    //             const addedPx = mousePosX * 30;
+    //             theImg.style.width = (800 + addedPx) + 'px';
+    //             prevLoc = mousePosX;
+    //             console.log(prevLoc);
+    //         }
+    // }
 
 
 })();
